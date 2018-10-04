0d99f0d2c7debe6eaee485e857cacdb582327125
Saha <windcrack@mail.ru>
2018-10-04 23:50:37 +0300

    Новые правки

--- lesson-01-extra/hardscr.js
+++ lesson-01-extra/hardscr.js
@@ -1,15 +1,17 @@
 'use strict';
 
-let num = 33721 * 2;
+let num = 3*3*7*2*1;
+
 console.log(num);
 
 function pow() {
   var result = num;
   for (let i = 1; i < 3; i++) {
     result *= num;
+    
   }
   return result;
 }
-console.log(pow());
+alert(pow());
 
 
---
+++ lesson-01/js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let money = prompt("Вашь бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+  moneyM: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenes: {},
+  income: [],
+  savings: true
+};
+console.log(appData.moneyM);
+console.log(appData.timeData);
+console.log(appData.expenses);
+console.log(appData.optionalExpenes);
+console.log(appData.income);
+console.log(appData.savings);
+let q1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+appData.expenses.quest1 = q1; 
+let a1 = prompt("Во сколько обойдется?", "");
+appData.expenses.answer1 = a1;
+let q2 = prompt("Введите обязательную статью расходов в этом месяце", "");
+appData.expenses.quest2 = q2; 
+let a2 = prompt("Во сколько обойдется?", "");
+appData.expenses.answer2 = a2;
+// let quest2 = prompt("Введите обязательную статью расходов в этом месяце", "");
+// let answer2 = prompt("Во сколько обойдется?", "");
+
+console.log(appData.expenses.quest1 + ":" + appData.expenses.answer1);
+console.log(appData.expenses.quest2 + ":" + appData.expenses.answer2);
+  let day = 30;
+function moneyDay(moneyDay){
+    moneyDay = appData.moneyM / day;
+   alert('Вашь бюджет на день ' + Math.ceil(moneyDay) + " рублей.");
+}
+moneyDay();
+
+
