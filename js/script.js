b9b70737109f4855b25d27b4f8b8c65ec463cb91
Saha <windcrack@mail.ru>
2018-10-04 16:09:41 +0300

    Мое домашнее задание

--- js/script.js
+++ js/script.js
@@ -1,58 +1,27 @@
 "use strict";
 
- let a = 5;
-
-console.log(a);
-
-var leftBorderWidth = 1;
-{
-  let second = 2;
-}
-
-const pi = 3.14;
-
-console.log(4/0);
-console.log('string' * 9);
-
-let somfing;
-console.log(somfing);
-
-let preson = {
-  name: 'John',
-  age: 25,
-  isMarried: false
+let moneyMonth = prompt("Вашь бюджет на месяц?", "");
+let nameShop = prompt("Название вашего магазина?", "");
+let mainList = {
+  moneyM: moneyMonth,
+  nameS: nameShop,
+  shopGoods: [],
+  employers: {},
+  open: true
 };
-
-console.log(preson ['name']);
-
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-
-console.log(arr[0]);
-
-// alert('Hello world');
-
-//  let answer=confirm('Are you here?');
-
-// console.log(answer);
-
-// let answer = +prompt('Есть ли вам 18?', 'Да');
-
-// console.log(typeof (answer));
-
-// console.log('arr'+' - object');
-// console.log(4 + +' - object');
-
-let incr = 10,
-    decr = 10;
-
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5%2);
-console.log("2" === 2);
-
-let isChecked = false,
-    isClose = false;
-
-console.log(isChecked || !isClose);
+console.log(mainList.moneyM);
+console.log(mainList.nameS);
+console.log(mainList.shopGoods);
+console.log(mainList.employers);
+console.log(mainList.open);
+for(let i = 0; i < 3; i++){
+  var typeArticle = prompt("Какой товар будем продовать?", "");
+  mainList.shopGoods[i] = typeArticle;
+}
+console.log(mainList.shopGoods);
+  let day = 30;
+function moneyDay(moneyDay){
+   moneyDay = mainList.moneyM / day;
+   document.write('Вашь бюджет на день ' + Math.ceil(moneyDay) + " рублей.");
+}
+moneyDay();
