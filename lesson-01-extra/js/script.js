41dd9c40e7967d5cd5024263356505eeeb5c8da0
Saha <windcrack@mail.ru>
2018-10-09 00:31:32 +0300

    Завтра до колупаю

--- lesson-01-extra/js/script.js
+++ lesson-01-extra/js/script.js
@@ -1,6 +1,6 @@
 'use strict';
-
-let num = 3*3*7*2*1;
+let num = 33721;
+    num = 3*3*7*2*1;
 
 console.log(num);
 
--- lesson-03-extra/js/script.js
+++ lesson-03-extra/js/script.js
@@ -1,2 +1,26 @@
-let str = "урок-3-был слишком легкий";
-    console.log()
+let str = "урок-3-был слишком легким";
+console.log(str.toUpperCase());
+let strSpace = str.replace(/-/g, " ");
+console.log(strSpace);
+let word = strSpace.replace(/урок 3 был слишком/g, "");
+console.log(word);
+console.log(word.replace(/им/g, "оо"));
+
+let arr = [20, 33, 1, "Человек", 2, 3];
+    arr.splice(3, 1);
+    for(let i = 0; i < arr.length; i++){
+       console.log(Math.cbrt(arr[i]));
+    }
+function textFifty(strdots) {
+    strdots = prompt("Введите строку более 50: ", "");
+    if (!isNaN(strdots)) {
+      alert("Вы ввели число!");
+    }
+    if(strdots.length > 50){
+      strdots.slice(0, 50) + '...';
+      console.log(strdots);
+    }
+    console.log(strdots.length); 
+}
+
+textFifty();
