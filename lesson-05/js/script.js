2f1e2fc3655fc845a07393679d7e33e6be362c2f
Saha <windcrack@mail.ru>
2018-10-12 00:07:37 +0300

    Моя мечта сбылась!

---
+++ lesson-05/js/script.js
@@ -0,0 +1,19 @@
+let menuItems = document.querySelectorAll('.menu-item'),
+    menu = document.querySelector('.menu');
+    li = document.createElement('li');
+    title = document.getElementById('title');
+    adv = document.querySelector('.adv');
+    prompt1 = document.querySelector('.prompt');
+
+menu.insertBefore(menuItems[2], menuItems[1]);
+li.classList.add('menu-item');
+li.textContent = 'Пятый пункт';
+document.body.style.background = 'url(img/apple_true.jpg)';
+menu.appendChild(li);
+title.textContent = 'Мы продаём только подлинную технику Apple';
+adv.style.display = 'none';
+let apple = prompt("Как вы относитесь к технике Apple?", '');
+prompt1.style.fontSize = "3rem";
+prompt1.textContent = apple;
+// menu.createElement()
+// console.log(li);
