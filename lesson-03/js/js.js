1c3e437efc043f34cdb24d3dbb940a52e37b4869
Saha <windcrack@mail.ru>
2018-10-24 01:38:38 +0300

    Promis

--- lesson-03/js/js.js
+++ lesson-03/js/js.js
@@ -282,3 +282,39 @@
 //     }
 //   });
 // });
+
+// lesson 12
+
+// let drink = 0;
+
+
+// function shoot(arrow) {
+//   console.log('Вы сделали выстрел...');
+//   let promise = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//       Math.random() > 0.5 ? resolve({}) : reject("Вы промахнулись");
+//     }, 3000);
+//   });
+//   return promise;
+// };
+
+// function win() {
+//   console.log('Вы победили!');
+//   (drink == 1) ? buyBeer(): giveMoney();
+// }
+
+// function loose() {
+//   console.log('Вы проиграли!')
+// }
+
+// function buyBeer() {
+//   console.log('Вам купили пиво');
+// }
+
+// function giveMoney() {
+//   console.log('Вам заплатили');
+// }
+// shoot({})
+//   .then(mark => console.log('Вы попали в цель'))
+//   .then(win)
+//   .catch(loose)
---
+++ lesson-12/js/script.js
@@ -0,0 +1,166 @@
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  let info = document.querySelector('.info-header'),
+    tab = document.querySelectorAll('.info-header-tab'),
+    tabContent = document.querySelectorAll('.info-tabcontent');
+
+  // Скрываем контетнт которые не должен отображатся
+  let hideTabContent = a => {
+    for (let i = a; i < tabContent.length; i++) {
+      tabContent[i].classList.remove('show');
+      tabContent[i].classList.add('hide');
+    }
+  }
+  // function hideTabContent(a) {
+  //   
+  // }
+  hideTabContent(1);
+  // Задем появление контента если он скрыт
+  let showTabContent = b => {
+    if (tabContent[b].classList.contains('hide')) {
+      tabContent[b].classList.remove('hide');
+      tabContent[b].classList.add('show');
+    }
+  }
+  // function showTabContent(b) {
+  //   
+  // }
+  // Устанавливаем событие при клике на определеную кнопку появлется соответсвующий контент
+  info.addEventListener('click', e => {
+    let target = e.target;
+    if (target && target.classList.contains('info-header-tab')) {
+      for (let i = 0; i < tab.length; i++) {
+        if (target == tab[i]) {
+          hideTabContent(0);
+          showTabContent(i);
+          break;
+        }
+      }
+    }
+  });
+  // Timer
+  let deadLine = '2018-10-25';
+
+  let getTimeRem = endtime => {
+    let t = Date.parse(deadLine) - Date.parse(new Date()),
+      sec = Math.floor((t / 1000) % 60),
+      min = Math.floor((t / 1000 / 60) % 60),
+      hou = Math.floor((t / (1000 * 60 * 60)));
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+    if (min < 10) {
+      min = "0" + min;
+    }
+    if (hou < 10) {
+      hou = "0" + hou;
+    }
+    return {
+      'total': t,
+      'hours': hou,
+      'minutes': min,
+      'second': sec
+    };
+  }
+  let setClock = (id, endtime) => {
+    let time = document.getElementById(id),
+      hours = time.querySelector('.hours'),
+      minutes = time.querySelector('.minutes'),
+      second = time.querySelector('.seconds');
+
+
+    let updateClock = () => {
+      let t = getTimeRem(endtime);
+      hours.textContent = t.hours;
+      minutes.textContent = t.minutes;
+      second.textContent = t.second;
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        hours.textContent = "00";
+        minutes.textContent = "00";
+        second.textContent = "00";
+      }
+
+    }
+    let timeInterval = setInterval(updateClock, 1000);
+  }
+  setClock('timer', deadLine);
+
+  // Modal
+  let body = document.querySelector('body'),
+    more = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay');
+
+  body.addEventListener('click', e => {
+    let target = e.target;
+    if (target.classList.contains('more')) {
+      overlay.style.display = 'block';
+      more.classList.add('more-splash');
+      body.style.overflow = 'hidden';
+    }
+    if (target.classList.contains('popup-close')) {
+      overlay.style.display = 'none';
+      more.classList.remove('more-splash');
+      body.style.overflow = '';
+    }
+    if (target.classList.contains('description-btn')) {
+      overlay.style.display = 'block';
+      more.classList.add('more-splash');
+      body.style.overflow = 'hidden';
+    }
+  });
+  // form
+
+  let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! скоро мы с вами свяжемся',
+    fail: 'Что то пошло не так...'
+  };
+
+  let form = document.getElementsByClassName('main-form')[0],
+      contatform = document.getElementById('form'),
+      input = form.getElementsByTagName('input'),
+      statusMesage = document.createElement('div');
+      statusMesage.classList.add('status');
+
+  function sendF(forms) {
+    forms.addEventListener('submit', e => {
+      e.preventDefault();
+      forms.appendChild(statusMesage);
+      let formsData = new FormData(forms);
+
+      function formPost(data) {
+        return new Promise((resolve, reject) => {
+          let request = new XMLHttpRequest();
+          request.open('POST', 'server.php');
+          request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+          request.onreadystatechange = () => {
+            if (request.readyState < 4) {
+              resolve();
+            } else if (request.readyState === 4) {
+              if (request.status == 200 && request.status < 300) {
+                resolve();
+              } else {
+                reject();
+              }
+            }
+          }
+          request.send(data);
+        })
+      }
+      function inputClear() {
+        for (let i = 0; i < input.length; i++) {
+          input[i].value = '';
+        }
+      }
+      formPost(formsData)
+        .then(() => statusMesage.innerHTML = message.loading)
+        .then(() => statusMesage.innerHTML = message.success)
+        .catch(() => statusMesage.innerHTML = message.fail)
+        .then(inputClear)
+    });
+  }
+  sendF(form);
+  sendF(contatform);
+});
